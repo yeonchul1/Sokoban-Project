7cd86fd25067a37db9389a53348c85f8399c2056
duscjf <duscjfgngn5@gmail.com>
2022-12-27 19:22:02 +0900

    프로그램 초기 세팅

--- ChoiYeonchul/Sokoban/Sokoban/Program.cs
+++ ChoiYeonchul/Sokoban/Sokoban/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 초기세팅
+Console.ResetColor();                                               // 컬러를 초기화.
+Console.CursorVisible = false;                                      // 커서를 숨긴다.
+Console.Title = "홍성재의 파이어펀치";                               // 타이틀을 설정한다.
+Console.BackgroundColor = ConsoleColor.DarkGray;                    // 배경색을 설정한다.
+Console.ForegroundColor = ConsoleColor.Yellow;                      // 글꼴색을 설정한다.
+Console.Clear();                                                    // 출력된 모든 내용을 지운다.
+
+
+int x = 10;
+int y = 5;
+Console.SetCursorPosition(x, y);
+Console.Write("H");
+
+Console.ReadLine();
+
+Console.WriteLine("Hello world");
